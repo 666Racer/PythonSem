60a91d0b74c964f153ee6fdacebce44980f23527
Misha <misha.hanzhin@gmail.com>
2023-11-02 22:39:35 +0500

    Закомментирован неверный код в TaskPy11

--- Sem3/TaskPy11.py
+++ Sem3/TaskPy11.py
@@ -8,15 +8,15 @@
 # Примечание: Пользователь может вводить значения
 # списка или список задан изначально.
 
-list_1 = [1,0,4,-1,-5,0,8,4,1]
-count = 0
-min_value = list_1[0]
-for i in list_1:
-    if i > min_value:
-        count +=1
-    i +=1
-print(f"Количество элементов, больших предыдущего, равно {count}")
+# list_1 = [1,0,4,-1,-5,0,8,4,1]
+# count = 0
+# min_value = list_1[0]
+# for i in list_1:
+#     if i+1 > min_value:
+#         count +=1
+#     i +=1
+# print(f"Количество элементов, больших предыдущего, равно {count}")
 
-# spisok = [4, 3, 5, 6, 1, 2]
-# chislo = [[spisok[i-1], spisok[i]] for i in range(1, len(spisok)) if spisok[i] > spisok[i-1]]
-# chislo2 = [print(' (' + str(item[0]) + ' < ' + str(item[1]) + ') ') for item in chislo]
+spisok = [4, 3, 5, 6, 1, 2]
+chislo = [[spisok[i-1], spisok[i]] for i in range(1, len(spisok)) if spisok[i] > spisok[i-1]]
+chislo2 = [print(' (' + str(item[0]) + ' < ' + str(item[1]) + ') ') for item in chislo]
